597324f894a10b9882a0d20be6a69ff49427a065
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-08 11:44:34 -0400

    Mall Maverick SDK 0.3  /mallmaverick.js

--- mallmaverick.js
+++ mallmaverick.js
@@ -388,14 +388,16 @@ function getBlogDataBySlug(slug){
     var mallDataJSON = JSON.parse(sessionStorage.mallData);
     var blog =  getObjects(mallDataJSON.blogs,'slug',slug)[0];
     var posts = [];
-    $.each(blog.posts, function(key, val){
-       var publish_date = new Date(val.publish_date);
-       var today = new Date();
-       if (publish_date <= today){
-           posts.push(val);
-       }
-    });
-    blog.posts = posts;
+    if(blog.posts.length > 0){
+        $.each(blog.posts, function(key, val){
+           var publish_date = new Date(val.publish_date);
+           var today = new Date();
+           if (publish_date <= today){
+               posts.push(val);
+           }
+        });
+        blog.posts = posts;
+    }
     return blog;
 }
 
