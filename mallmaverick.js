a3ea114bbf5a93019da191528c103899801d188c
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-13 11:51:32 -0400

    Mall Maverick SDK 0.3  /mallmaverick.js

--- mallmaverick.js
+++ mallmaverick.js
@@ -93,7 +93,7 @@ function get_instagram(url,total, callback){
             if (i < total){
                 
                 var ig_rendered =  Mustache.render(html,feed_obj);
-                console.log(ig_rendered)
+                console.log(unescape(ig_rendered))
                 item_rendered.push(unescape(ig_rendered.trim()));
             }
         })
