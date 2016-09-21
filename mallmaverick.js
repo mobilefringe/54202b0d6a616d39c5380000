ee68a69e02a0a44c94e4ba7e0871d0e3d8dae481
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-09-21 11:28:31 -0400

    Mall Maverick SDK 0.3  /mallmaverick.js

--- mallmaverick.js
+++ mallmaverick.js
@@ -163,7 +163,9 @@ function localizeObject(mm_object){
             if(mm_object.description_2 !== null && typeof(mm_object.description_2) != 'undefined' && mm_object.description_2.length > 0){
                 mm_object.description = mm_object.description_2;
             }
-            
+             if(mm_object.description_short_2 !== null && typeof(mm_object.description_short_2) != 'undefined' && mm_object.description_short_2.length > 0){
+                mm_object.description = mm_object.description_short_2;
+            }
             
             if(mm_object.rich_description_2 !== null && typeof(mm_object.rich_description_2) != 'undefined' && mm_object.rich_description_2.length > 0){
                 mm_object.rich_description = mm_object.rich_description_2;
