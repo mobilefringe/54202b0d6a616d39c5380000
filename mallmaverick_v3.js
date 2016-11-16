c333023df7058040a8474d1e12d2b6e59e4618ee
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-11-16 11:35:26 -0500

    Mall Maverick SDK 0.3  /mallmaverick_v3.js

--- mallmaverick_v3.js
+++ mallmaverick_v3.js
@@ -1289,5 +1289,19 @@ function isEmpty(obj) {
     };
 })();
 
-
-	
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
